3051e600d9c8de066ecb681ee15ac14f2cad0139
MayangAsura <mayang.asura123@gmail.com>
2026-07-01 03:08:38 +0700

    updated cors config, modified vercel.json

--- app.js
+++ app.js
@@ -43,7 +43,14 @@ const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001', 'http:
 // }
 
 const corsOptions = {
-  origin: allowedOrigins,
+  origin: (origin, callback) => {
+    if(allowedOrigins.indexOf(origin != -1 || !origin)){
+      callback(null, true)
+    }else{
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  optionsSuccessStatus: 200,
   credentials: true
 };
 
