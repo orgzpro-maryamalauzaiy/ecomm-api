23db57707b42b2bd98ef085066da61b06bff1e8c
MayangAsura <mayang.asura123@gmail.com>
2026-07-02 12:01:13 +0700

    updated postgres connection ca filepath

--- config/postgres.js
+++ config/postgres.js
@@ -2,6 +2,7 @@ const pkg = require("pg")
 // import pkg from "pg"
 const dotenv = require('dotenv')
 const fs = require('fs')
+const path = require('path')
 // const DB_URI = process.env.DB_PG_URI
 // const connectionString = DB_URI
 
@@ -12,6 +13,9 @@ dotenv.config()
 //   connectionString
 // })
 
+const ca_fileName = "../ca-cert-postgres-database/ca.pem"
+const ca_filePath = path.join(__dirname, ca_fileName)
+
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
@@ -20,7 +24,22 @@ const pool = new Pool({
   port: process.env.DB_PORT,
   ssl: {
     rejectUnauthorized: true,
-    ca: fs.readFileSync('../ca-cert-postgres-database/ca.pem').toString()
+    ca: fs.readFileSync(ca_filePath, function(error, data) {
+      if(error){
+        console.log(error)
+        return
+      }
+
+      return data.toString()
+    })
+    // ca: fs.readFileSync('./ca-cert-postgres-database/ca.pem', function(error, data) {
+    //   if(error){
+    //     console.log(error)
+    //     return
+    //   }
+
+    //   return data.toString()
+    // })
     // ca: fs.readFileSync('../ca-cert-postgres-database/ca.pem').toString()
   }
 })
